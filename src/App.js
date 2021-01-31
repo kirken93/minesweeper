12a67dfd907784a92eacf8c84d184eff4ee9bc07
Megan Kirkbride <megan.kirkbride@bloomerang.co>
2021-01-30 23:15:57 -0500

    Adding text field and inputs to make the game configurable

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import { useState } from "react";
 import './App.css';
 import BoardModel from "./BoardModel.js";
 import Board from "./Board.jsx";
+import TextField from "./TextField"
 
-// TODO make these configurable
-const height = 10;
-const width = 10;
-const numBombs = 10;
-const createNewBoard = () => { return new BoardModel({ height, width, numBombs }) };
+const createNewBoard = (height, width, numBombs) => {
+  if (height * width < numBombs) {
+    alert("Cannot have more mines than squares");
+    return null;
+  }
+
+  return new BoardModel({ height, width, numBombs });
+};
 
 function App() {
-  const [board, setBoard] = useState(createNewBoard());
-  const newGame = () => setBoard(createNewBoard());
+  const [height, setHeight] = useState(10);
+  const [width, setWidth] = useState(10);
+  const [numBombs, setNumBombs] = useState(10);
+
+  const [board, setBoard] = useState(createNewBoard(height, width, numBombs));
+  const newGame = () => {
+    setBoard(createNewBoard(height, width, numBombs));
+  };
 
   return (
     <div className="App">
       <Board board={board} setBoard={setBoard} newGame={newGame} />
+      <div>
+        <TextField label="Height"
+                   value={height}
+                   onChange={setHeight}
+                   id="height" />
+        <TextField label="Width"
+                   value={width}
+                   onChange={setWidth}
+                   id="width" />
+        <TextField label="Bombs"
+                   value={numBombs}
+                   onChange={setNumBombs}
+                   id="bombs" />
+      </div>
     </div>
   );
 }
